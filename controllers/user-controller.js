25726beeac3e985aa4ed33d85244fc300f524500
Napo <Michaeljnapolitano@gmail.com>
2020-12-11 20:35:14 -0500

    trying .remove to remove thoughts when user deleted

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,20 +3,20 @@ const { User } = require("../models");
 const userController = {
   getAllUsers(req, res) {
     User.find({})
-    .select("-__v")
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => {
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
         console.log(err);
-        res.status(400).json(err)
-    })
+        res.status(400).json(err);
+      });
   },
 
   // find one user by ID
   getUserById(req, res) {
-      User.findOne({ _id: req.params.id})
+    User.findOne({ _id: req.params.id })
       .populate({
-          path: "thoughts",
-          select: "-__v",
+        path: "thoughts",
+        select: "-__v",
       })
       .select("-__v")
       .then((dbUserData) => {
@@ -35,14 +35,17 @@ const userController = {
 
   // create user
   createUser({ body }, res) {
-      User.create(body)
+    User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
 
   // update User by id
   updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
       .then((dbUserData) => {
         if (!dbUserData) {
           res.status(404).json({ message: "No user found with this id!" });
@@ -56,6 +59,9 @@ const userController = {
   // delete User
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.id })
+      .remove({
+        path: "thoughts",
+      })
       .then((dbUserData) => {
         if (!dbUserData) {
           res.status(404).json({ message: "No user found with this id!" });
